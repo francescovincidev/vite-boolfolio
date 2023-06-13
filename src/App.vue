571c9e44bbfda53715bc3b07bc1f36105459a0cd
francescovincidev <francescovinci2000@gmail.com>
2023-06-13 23:44:44 +0200

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <script>
+import axios from 'axios';
 
 export default {
+    data() {
+        return {
+            projects: []
+        }
+
+    },
+    mounted() {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
+                this.projects = resp.data.results;
+            })
+        }
+    }
 
 }
 
